8914c95e13b6800decf73453beba7b928b62285a
Martín Tamashiro <martintama@gmail.com>
2015-09-08 09:25:45 -0300

    Added some images and navigation

--- app/src/main/java/com/inclutec/louis/fragments/ExerciseFragment.java
+++ app/src/main/java/com/inclutec/louis/fragments/ExerciseFragment.java
@@ -7,11 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.inclutec.louis.R;
+import com.inclutec.louis.lib.BrailleCellHandler;
+import com.inclutec.louis.util.ExerciseType;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ExerciseFragment extends Fragment {
+
+     BrailleCellHandler brailleCellHandler;
 
     public ExerciseFragment() {
     }
@@ -19,6 +23,32 @@ public class ExerciseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_exercise, container, false);
+        View inflatedView = inflater.inflate(R.layout.fragment_exercise, container, false);
+
+        setListeners(inflatedView);
+
+        return inflatedView;
+    }
+
+    public void setListeners(View container){
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.btnNext:
+
+                        break;
+                    case R.id.btnResend:
+
+                        break;
+
+                }
+            }
+        };
+
+        container.findViewById(R.id.btnNext).setOnClickListener(buttonClickListener);
+        container.findViewById(R.id.btnResend).setOnClickListener(buttonClickListener);
+
     }
 }
